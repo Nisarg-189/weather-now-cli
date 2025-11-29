78f181f411c61a087cf1df1c9cfe5566fc567b5a
Nisarg-189 <nis.1.8.9.1.8.9@gmail.com>
2025-11-29 16:21:44 +0530

    Add weather app project

---
+++ main.py
@@ -0,0 +1,24 @@
+from weather.api import get_weather
+
+def main():
+
+    print("==== Weather App ====")
+
+    lat = float(input("Enter latitude: "))
+    lon = float(input("Enter longitude: "))
+
+    weather = get_weather(lat,lon)
+
+    if "error" in weather:
+        print("Something went wrong! ", weather["error"])
+
+    else:
+        print("---- Current Weather ----")
+        print("Temperature: ", weather["temperature"], "°C")
+        print("Wind Speed: ", weather["windspeed"], "km/h")
+        print("Wind Direction: ", weather["winddirection"], "°")
+        print("Time: ", weather["time"])
+
+
+if __name__ == "__main__":
+    main()
